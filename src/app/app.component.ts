50102d9e1ee5a4bedf0eacc2fcc46675648b0fea
ilyas cherfi <ilyascherfi91@gmail.com>
2024-08-20 12:35:41 +0200

    call face-snap component in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    FaceSnapComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
