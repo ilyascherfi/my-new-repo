49941574ab0c7bb56b4ca7ef43aa7b58cea7b29b
ilyas cherfi <ilyascherfi91@gmail.com>
2024-09-15 16:05:54 +0200

    add 2 other faceSnap component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ import { FaceSnap } from './models/face-snap';
 })
 export class AppComponent implements OnInit {
   mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
   ngOnInit(): void {
     this.mySnap = new FaceSnap(
       "Archibald",
@@ -20,6 +22,20 @@ export class AppComponent implements OnInit {
       "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
       new Date(),
       10
-    )
+    );
+    this.myOtherSnap = new FaceSnap(
+      "John Sourdough",
+      "Born and raised by jin jocks!",
+      "https://images.unsplash.com/photo-1682786308235-d3e74451a32c?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      new Date(),
+      8
+    );
+    this.myLastSnap = new FaceSnap(
+      "Une meuf ",
+      "Dans une bibliothèque avec une apple watch",
+      "https://plus.unsplash.com/premium_photo-1681681061526-5f5496c635e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      new Date(),
+      123
+    );
   }
 }
