df069f8c6aee7d0ec6111244164d861c53c93f6c
ilyas cherfi <ilyascherfi91@gmail.com>
2024-09-15 16:09:50 +0200

    add location to face-snap model, and use it in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,9 @@ export class AppComponent implements OnInit {
       "Dans une bibliothèque avec une apple watch",
       "https://plus.unsplash.com/premium_photo-1681681061526-5f5496c635e1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       new Date(),
-      123
+      123,
     );
+
+    this.myOtherSnap.setLocation("Phto à la montagne")
   }
 }
