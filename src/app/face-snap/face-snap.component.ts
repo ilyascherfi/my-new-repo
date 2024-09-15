bc059196d89aa39f55f3e2203f9a0f15a42660cf
ilyas cherfi <ilyascherfi91@gmail.com>
2024-09-15 18:32:00 +0200

    add date pipe

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
-import { NgStyle, NgClass, UpperCasePipe, LowerCasePipe, TitleCasePipe} from '@angular/common';
+import { NgStyle, NgClass, UpperCasePipe, LowerCasePipe, TitleCasePipe, DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
   imports: [
-    NgStyle, NgClass, UpperCasePipe, LowerCasePipe, TitleCasePipe
+    NgStyle, NgClass, UpperCasePipe, LowerCasePipe, TitleCasePipe, DatePipe
   ],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
